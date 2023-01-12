47893e5cf48aa778e3feb36cdb4bbd5407feb61d
oleg3007 <oleg_bitelev@mail.ru>
2023-01-12 06:35:09 +0300

    Окончание

---
+++ script.js
@@ -0,0 +1,45 @@
+
+let page = document.querySelector('.page');
+let main = page.querySelector('.main');
+let profile = main.querySelector('.profile');
+let profileEditButton = profile.querySelector('.profile__edit-button');
+let popupButton = document.querySelector('.popup__button');
+let popup = document.querySelector('.popup');
+let popupCros = popup.querySelector('.popup__cros');
+
+
+let elements = main.querySelector('.elements');
+let elementGroup = elements.querySelectorAll('.element .element__rectangle .element__group');
+
+function popupActiv (){
+	popup.classList.toggle('popup__opened');
+	popup.classList.toggle('popup');
+}
+
+function handleFormSubmit(evt) {
+	let popupName = popup.querySelector('.popup__name').value;
+	let popupYourHobby = popup.querySelector('.popup__your-hobby').value;
+	let profileTittle = document.querySelector('.profile__tittle');
+	let profileText = document.querySelector('.profile__text');
+	profileTittle.innerHTML = popupName;
+	profileText.innerHTML = popupYourHobby;
+}
+
+popupButton.addEventListener('click', function(){
+	handleFormSubmit();
+	popupActiv();
+});
+
+popupCros.onclick = popupActiv;
+profileEditButton.onclick = popupActiv;
+
+
+
+for (i = 0; i < elementGroup.length; i++){
+	elementGroup[i].onclick = clickGroup;
+}
+
+function clickGroup(){
+	this.classList.toggle('element__group_black');
+}
+elementGroup.onclick = clickGroup;
