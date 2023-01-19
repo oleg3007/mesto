daef63739034d23fbed3ed2b49cdefa770f82180
oleg3007 <oleg_bitelev@mail.ru>
2023-01-19 17:40:29 +0300

    fix: Поправки в script

--- script/script.js
+++ script/script.js
@@ -6,39 +6,29 @@ let profileEditButton = profile.querySelector('.profile__edit-button');
 let popupButton = document.querySelector('.popup__button');
 let popup = document.querySelector('.popup');
 let popupCros = popup.querySelector('.popup__cros');
+let profileTittle = document.querySelector('.profile__tittle');
+let profileText = document.querySelector('.profile__text');
+let formElement = document.querySelector('.popup__conteiner');
 
-
-let elements = main.querySelector('.elements');
-let elementGroup = elements.querySelectorAll('.element .element__group');
-
-function popupActiv (){
-	popup.classList.toggle('popup-opened');
+function popupActiv (evt){
+	evt.preventDefault();
+	popup.querySelector('.popup__hield_name').value = profileTittle.textContent;
+	popup.querySelector('.popup__hield_About-me').value = profileText.textContent;
+	popup.classList.add('popup-opened');
 }
 
 function handleFormSubmit(evt) {
-	let popupName = popup.querySelector('.popup__hield_name').value;
-	let popupYourHobby = popup.querySelector('.popup__hield_About-me').value;
-	let profileTittle = document.querySelector('.profile__tittle');
-	let profileText = document.querySelector('.profile__text');
-	profileTittle.textContent = popupName;
-	profileText.textContent = popupYourHobby;
+	evt.preventDefault();
+	profileTittle.textContent = popup.querySelector('.popup__hield_name').value;
+	profileText.textContent = popup.querySelector('.popup__hield_About-me').value;
+	popup.classList.remove('popup-opened');
+
+}
+function popupRemove(evt){
+	evt.preventDefault();
+	popup.classList.remove('popup-opened');
 }
 
-popupButton.addEventListener('click', function(){
-	handleFormSubmit();
-	popupActiv();
-});
-
-popupCros.onclick = popupActiv;
+popupCros.onclick = popupRemove;
 profileEditButton.onclick = popupActiv;
-
-
-
-for (i = 0; i < elementGroup.length; i++){
-	elementGroup[i].onclick = clickGroup;
-}
-
-function clickGroup(){
-	this.classList.toggle('element__group_color_black');
-}
-elementGroup.onclick = clickGroup;
+formElement.addEventListener('submit', handleFormSubmit); 
