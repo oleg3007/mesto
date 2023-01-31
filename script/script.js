0f0a44172fc5f119806225e6e5d578d701d3c745
oleg3007 <oleg_bitelev@mail.ru>
2023-01-31 15:49:57 +0300

    feat: Добавлено popup окно карточки

--- script/script.js
+++ script/script.js
@@ -5,11 +5,13 @@ let profile = main.querySelector('.profile');
 let profileEditButton = profile.querySelector('.profile__edit-button');
 let popupButton = document.querySelector('.popup__button');
 let popup = document.querySelector('.popup');
+let opopupItem = document.querySelector('.popup-item');
 let popupCros = popup.querySelector('.popup__cros');
 let popupHieldName = popup.querySelector('.popup__hield_enter_name');
 let popupHieldAboutMe = popup.querySelector('.popup__hield_enter_about-me');
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
+let profileAddButton = profile.querySelector('.profile__add-button');
 let formElement = document.querySelector('.popup__conteiner');
 
 function popupActiv (){
@@ -21,6 +23,10 @@ function popupActiv (){
 
 function popupRemove() {
 	popup.classList.remove('popup_active');
+	opopupItem.classList.remove('popup_active');
+}
+function popupItemActiv() {
+	opopupItem.classList.add('popup_active');
 }
 
 function handleFormSubmit(evt) {
@@ -31,5 +37,7 @@ function handleFormSubmit(evt) {
 }
 
 popupCros.onclick = popupRemove;
+opopupItem.onclick = popupRemove;
 profileEditButton.onclick = popupActiv;
 formElement.addEventListener('submit', handleFormSubmit); 
+profileAddButton.addEventListener('click', popupItemActiv);
