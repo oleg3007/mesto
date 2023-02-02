20794313aa4fc5527601be7c70df0dabb4bcfbdf
oleg3007 <oleg_bitelev@mail.ru>
2023-02-02 14:27:48 +0300

    feat: создание карточки товара

--- script/script.js
+++ script/script.js
@@ -1,18 +1,25 @@
 
 let page = document.querySelector('.page');
 let main = page.querySelector('.main');
+
 let profile = main.querySelector('.profile');
-let profileEditButton = profile.querySelector('.profile__edit-button');
-let popupButton = document.querySelector('.popup__button');
-let popup = document.querySelector('.popup');
-let opopupItem = document.querySelector('.popup-item');
-let popupCros = popup.querySelector('.popup__cros');
-let popupHieldName = popup.querySelector('.popup__hield_enter_name');
-let popupHieldAboutMe = popup.querySelector('.popup__hield_enter_about-me');
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
 let profileAddButton = profile.querySelector('.profile__add-button');
+let profileEditButton = profile.querySelector('.profile__edit-button');
+
+let popupButton = document.querySelector('.popup__button');
+let popup = document.querySelector('.popup');
+let popupCros = popup.querySelector('.popup__cros');
+let popupHieldName = popup.querySelector('.popup__hield_enter_name');
+let popupHieldAboutMe = popup.querySelector('.popup__hield_enter_about-me');
 let formElement = document.querySelector('.popup__conteiner');
+
+let popupCrosItem = document.querySelector('.popup-item__cros');
+let popupItem = document.querySelector('.popup-item');
+let popupItemButton = document.querySelector('.popup-item__button');
+
+let elements = main.querySelector('.elements');
 
 function popupActiv (){
 	popupHieldName.value = profileTitle.textContent;
@@ -23,10 +30,10 @@ function popupActiv (){
 
 function popupRemove() {
 	popup.classList.remove('popup_active');
-	opopupItem.classList.remove('popup_active');
+	popupItem.classList.remove('popup_active');
 }
 function popupItemActiv() {
-	opopupItem.classList.add('popup_active');
+	popupItem.classList.add('popup_active');
 }
 
 function handleFormSubmit(evt) {
@@ -36,8 +43,31 @@ function handleFormSubmit(evt) {
 	popupRemove();
 }
 
+function addElement (linkElement, titleElement) {
+	const eventCard = document.querySelector('#event-card').content; // Находим заготовленый блок
+	const element = eventCard.querySelector('.element').cloneNode(true); // Кланируем его
+
+	element.querySelector('.element__mask-group').src = linkElement; // заполняем
+	element.querySelector('.element__title').textContent = titleElement;
+
+	// songElement.querySelector('.song__like').addEventListener('click', function (evt) { // На сердечко повесили событие
+	// 	//console.log(event);
+	// 	evt.target.classList.toggle('song__like_active');
+	// });	
+	elements.prepend(element);  // .... и ставим в конец контейнера.
+	popupRemove();
+}
+
 popupCros.onclick = popupRemove;
-opopupItem.onclick = popupRemove;
+popupCrosItem.onclick = popupRemove;
 profileEditButton.onclick = popupActiv;
 formElement.addEventListener('submit', handleFormSubmit); 
 profileAddButton.addEventListener('click', popupItemActiv);
+popupItemButton.addEventListener('click', function (){
+	const popupItemTitle = popupItem.querySelector('.popup__hield_enter_title');
+	const popupItemLink = popupItem.querySelector('.popup__hield_enter_link');
+	addElement(popupItemLink.value, popupItemTitle.value)
+	popupItemTitle.value = '';
+	popupItemLink.value = '';
+});
+
