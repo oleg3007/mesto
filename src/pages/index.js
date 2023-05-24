f1a19aafd71425a07dfd7121fc0cc9275fa56c0c
oleg3007 <oleg_bitelev@mail.ru>
2023-05-24 23:47:57 +0300

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { getItems } from '../components/api.js';
+
 
 import './index.css';
 import Card from '../components/Card.js';
@@ -23,7 +23,6 @@ import {
 	configCard
 } from '../utils/constants.js';
 
-// getItems();
 
 const validatorPopupItem = new FormValidator(popupItem, config);
 validatorPopupItem.enableValidation();
